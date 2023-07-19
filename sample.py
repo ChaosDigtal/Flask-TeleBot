24dbaaf91a5413585285b263e135c0fa85e1aea9
HirukiDev <hiruki.master.dev@gmail.com>
2023-07-20 03:21:07 +0900

    fadsadfs

--- sample.py
+++ sample.py
@@ -335,7 +335,8 @@ def finishTransaction(message, budget, crypto, message_id):
     if message.text == "/start":
         bot.send_message(message.chat.id, "What would you like to do?", reply_markup=gen_menu())
         return
-    bot.send_message(message.chat.id, text="Thanks for your investment!!!\n\nYour submission is pending and will be confirmed and accepted soon!\n\n/start command for menu!")
+    icrypto = int(crypto)
+    bot.send_message(message.chat.id, text="Dear {message.from_user.first_name}!\n\nYou have just made an amazing investment!\n\n   Amount: {budget}$'\n\n   Crypto: {cryptos[icrypto]}\n\n Many thanks for your investment!!!\n\nYour submission is now inserted into the pending list\n\nThe administrator will check your transaction if valied and will let you know soon!\n\nPlease remember it won't take no more than 4 hours!\n\nYou will be noticed whether your submission is rejected or accepted.\n\nOnce rejected, you may check your transaction hash value and DM @fly15support!\n\nOnce accepted, you can enjoy a grate economic trip with our Fly15!\n\nEvery 24 hours, you will be able to withdraw exactly 15% of your initial deposited.\n\nYou may check this in 'Withdraw' panel from start menu!\n\nHope you enjoy with our Fly15!\n\nThanks again,\n\n\n\n/start command for menu!")
     if message.from_user.username != 'aless000000' and message.from_user.username != 'fly15support':
         bot.edit_message_text(chat_id=message.chat.id, message_id=message_id, text=f'Please deposit to the following address:\n\n{wallet[int(crypto)]}\n\nOnce you deposit, input your transaction hash to finish your investment')
         return
