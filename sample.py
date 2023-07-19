c647661591a1b17a3835ed9915bb39ca80418b1b
HirukiDev <hiruki.master.dev@gmail.com>
2023-07-19 13:57:00 +0900

    kick-comp

--- sample.py
+++ sample.py
@@ -306,7 +306,13 @@ def callback_query(call):
         })
         bot.send_message(call.message.chat.id, "Your request has been sent successfully!\n\nPlease wait for acception!")
 #5939115860
-bot.kick_chat_member('5939115860', 'CreativeDev0809')
+chat_id = '5939115860'
+user_to_remove = 'CreativeDev0809'
+chat_member = bot.get_chat_member(chat_id, user_to_remove)
+user_id = chat_member.user.id
+
+bot.kick_chat_member(int(chat_id), int(user_id))
+
 @bot.message_handler(commands=['start'])
 def message_handler(message):
     print(message.chat.id)
